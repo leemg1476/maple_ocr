697ad0c27d746ae5fe66dcc08b5275a19c6e29a3
LeeMyeonggyu <si44440@naver.com>
2022-03-07 14:31:53 +0900

    메이플 일퀘 인식 성공

--- venv/ocr.py
+++ venv/ocr.py
@@ -1,7 +1,13 @@
 from PIL import Image
 from pytesseract import *
+import matplotlib.pylab as plt
 
-image = Image.open('image_04.jpg')
+image = Image.open('image_05.jpg')
+size = image.size
+image = image.resize((size[0]*3,size[1]*3))
 
-print_image = image_to_string(image, lang='eng')
+plt.imshow(image)
+plt.show()
+
+print_image = image_to_string(image, lang='kor')
 print(print_image)
